75f291753f05870677d14c8eae769aadbea24a61
aalvaropc <aalvaropc@gmail.com>
2024-02-09 11:41:05 -0500

    first commit

---
+++ src/components/ChatWindow.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from "react";
+import dayjs from "dayjs";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import MDEditor from "@uiw/react-md-editor";
+import InputBox from "./InputBox";
+
+import "../assets/ChatWindow.css";
+import logo from "../assets/img/gemini-small.png";
+
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 id="chat-header">
+        <img src={logo} alt="gemini" width={120} />
+        <b style={{ marginLeft: 5 }}>Chatbot</b>
+      </h1>
+      <small>Practicando con Gemini en ReactJS</small>
+    </div>
+  );
+};
+
+const ChatWindow = () => {
+  const chatContainerRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+  }, [messages]);
+
+  const sendMessage = async (inputText) => {
+    if (!inputText) {
+      return;
+    }
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: inputText, sender: "user", timestamp: new Date() },
+    ]);
+
+    setLoading(true);
+
+    try {
+      const result = await model.generateContent(inputText);
+      const text = result.response.text();
+      const isCode = text.includes("```");
+
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          text: text,
+          sender: "ai",
+          timestamp: new Date(),
+          isCode,
+        },
+      ]);
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("generateContent error: ", error);
+    }
+  };
+
+  return (
+    <div className={`chat-window`}>
+      <Header />
+      <div className="chat-container" ref={chatContainerRef}>
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`message ${message.sender === "user" ? "user" : "ai"}`}
+          >
+            {message.isCode ? (
+              <MDEditor.Markdown
+                source={message.text}
+                style={{ whiteSpace: "pre-wrap" }}
+              />
+            ) : (
+              <>
+                <p className="message-text">{message.text}</p>
+                <span
+                  className={`time ${
+                    message.sender === "user" ? "user" : "ai"
+                  }`}
+                >
+                  {message.timestamp
+                    ? dayjs(message.timestamp).format("DD.MM.YYYY HH:mm:ss")
+                    : ""}
+                </span>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+      <InputBox sendMessage={sendMessage} loading={loading} />
+    </div>
+  );
+};
+
+export default ChatWindow;
